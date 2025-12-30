ae06a4f5e9215758ed45283b058ca58cc3879074
LaryssaPam <laryssapam6@gmail.com>
2025-12-30 08:45:10 -0400

    push

--- database/migrations/2025_12_30_123244_create_posts_table.php
+++ database/migrations/2025_12_30_123244_create_posts_table.php
@@ -12,7 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
+            $table->string('title');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->text('content');
+            $table->string('author');
+            $table->boolean('published')->default(false);
             $table->timestamps();
         });
     }
