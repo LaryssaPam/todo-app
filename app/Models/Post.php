3e8667f4b29fd1184a34d691a56bb44f2d90cd0c
LaryssaPam <laryssapam6@gmail.com>
2025-12-30 08:50:34 -0400

    modification du modele

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
+// $fillable pour sécurisé le droit d'ecriture
 {
-    //
+    use HasFactory;
+    
+    protected $fillable = [
+        'title',
+        'content',
+        'author',
+        'published'
+    ];
+
+    protected $casts = [
+        'published' => 'boolean',
+    ];
 }
